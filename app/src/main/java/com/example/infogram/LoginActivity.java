e39816404d75af515a7b0028cd865405c5a8189c
FranciscoETSD <tadeo00704@gmail.com>
2021-08-26 08:05:02 -0400

    CoordinatorLayout y TextInputEditText

--- app/src/main/java/com/example/infogram/LoginActivity.java
+++ app/src/main/java/com/example/infogram/LoginActivity.java
@@ -1,7 +1,8 @@
 package com.example.infogram;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class LoginActivity extends AppCompatActivity {
 
