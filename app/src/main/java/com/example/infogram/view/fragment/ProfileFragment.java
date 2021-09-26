64741b20c49bc2120b53940ebe9df8805b4d6797
FranciscoETSD <tadeo00704@gmail.com>
2021-09-26 17:36:51 -0400

    CircleImageView y SimpleViewBehavior

--- app/src/main/java/com/example/infogram/view/fragment/ProfileFragment.java
+++ app/src/main/java/com/example/infogram/view/fragment/ProfileFragment.java
@@ -2,13 +2,21 @@ package com.example.infogram.view.fragment;
 
 import android.os.Bundle;
 
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.infogram.R;
+import com.example.infogram.adapter.CardViewAdapter;
+import com.example.infogram.model.Image;
+
+import java.util.ArrayList;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -61,6 +69,49 @@ public class ProfileFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile, container, false);
+        View view = inflater.inflate(R.layout.fragment_profile, container, false);
+
+        //toolbar
+        showToolBar("", false,view);
+
+        //recyclerview
+        RecyclerView recyclerView = view.findViewById(R.id.recycler_view_profile);
+
+        //Layout manager
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(getContext());
+        linearLayoutManager.setOrientation(LinearLayoutManager.VERTICAL);
+        recyclerView.setLayoutManager(linearLayoutManager);
+
+        //el adapter
+        CardViewAdapter cardViewAdapter = new CardViewAdapter(buidImages(), R.layout.cardview_image, getActivity());
+        recyclerView.setAdapter(cardViewAdapter);
+
+        return view;
+    }
+    public void showToolBar(String titulo, boolean botonSubir, View view){
+        Toolbar toolbar = view.findViewById(R.id.toolbar_profile);
+        ((AppCompatActivity) getActivity()).setSupportActionBar(toolbar);
+
+        ((AppCompatActivity) getActivity()).getSupportActionBar().setTitle(titulo);
+        ((AppCompatActivity) getActivity()).getSupportActionBar().setDisplayHomeAsUpEnabled(botonSubir);
+    }
+
+    //creamos la lista de imagenes
+    public ArrayList<Image> buidImages(){
+        ArrayList<Image> images = new ArrayList<>();
+
+        images.add(new Image("https://cronicoviajero.com/wp-content/uploads/DSCN1516-scaled.jpg", "Carlos Valdivia", "2 dias","1 me gusta"));
+        images.add(new Image("https://laicismo.org/wp-content/uploads/2017/02/Monumento-Virgen-de-Socavon-en-Oruro-Bolivia.jpg", "Luis Perez", "3 dias","5 me gusta"));
+        images.add(new Image("https://serturista.com/wp-content/uploads/2014/01/Castillo-de-la-Glorieta-2.jpg", "Fernando Torrico", "6 dias","4 me gusta"));
+        images.add(new Image("https://www.laregion.bo/wp-content/uploads/2019/07/coroico-parapente.jpeg", "Maria Flores", "5 dias","6 me gusta"));
+        images.add(new Image("https://www.wamanadventures.com/blog/wp-content/uploads/2019/06/La-Laguna-Colorada-Waman-Adventures.jpg", "Rolando Ramos", "3 dias","2 me gusta"));
+        images.add(new Image("https://assets.cdnelnuevodiario.com/photos/0a8825a02c9611e893420eb04a1bba78.jpg", "William Jaimes", "4 dias","5 me gusta"));
+        images.add(new Image("https://i2.wp.com/andorreandoporelmundo.com/wp-content/uploads/2019/02/IMG_5577.jpg?fit=1000%2C667&ssl=1", "Roberto Paniagua", "2 dias","3 me gusta"));
+        images.add(new Image("https://www.lostiempos.com/sites/default/files/styles/noticia_detalle/public/media_imagen/2018/1/13/1-_roma_2-_hector_arnez.jpg?itok=UCQEK8Mi", "Lorena Herrera", "2 dias","8 me gusta"));
+        images.add(new Image("https://media-cdn.tripadvisor.com/media/photo-s/0e/1b/f4/2a/fuerte-de-samaipata.jpg", "Laura Caceres", "6 dias","3 me gusta"));
+        images.add(new Image("https://i1.wp.com/elcalderoviajero.com/wp-content/uploads/2018/04/bolivia-cochabamba-incachaca-15.jpg?resize=750%2C563&ssl=1", "Francisco Eduardo Tadeo", "1 dias","34 me gusta"));
+
+
+        return images;
     }
 }
